79827a1bf2b8f3d3301f60b32aacc7235a30227e
alican-zeybel <alican.zeybel112@gmail.com>
2022-11-24 19:23:42 +0300

    Coffe project added.

--- src/chapter2/MadLibs.java
+++ src/chapter2/MadLibs.java
@@ -3,17 +3,24 @@ package chapter2;
 import java.util.Scanner;
 
 public class MadLibs {
+
     public static void main(String[] args){
 
-        System.out.println("Please enter year:  ");
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter year:  ");
+
         int year = scanner.nextInt();
+
         System.out.println("How many cup you drink:  ");
         int cup = scanner.nextInt();
+
         System.out.println("Please enter adjective:  ");
         String adj = scanner.next();
         scanner.close();
-        System.out.println("On a "+adj+" "+year+" day, I drink a minimum of "+cup+" cups of coffee ");
+        System.out.println("On a "+adj+" "+year+
+                " day, I drink a minimum of "+cup+
+                " cups of coffee ");
 
     }
 }
